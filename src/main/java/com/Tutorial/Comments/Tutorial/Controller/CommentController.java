df5ad94dcbec96429a12a67ade00b68bc961314c
William Spearman <wspearman@protonmail.com>
2022-10-08 16:01:41 -0400

    Changed to port 8080

--- src/main/java/com/Tutorial/Comments/Tutorial/Controller/CommentController.java
+++ src/main/java/com/Tutorial/Comments/Tutorial/Controller/CommentController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping
 public class CommentController {
--- src/main/java/com/Tutorial/Comments/Tutorial/Controller/TutorialController.java
+++ src/main/java/com/Tutorial/Comments/Tutorial/Controller/TutorialController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api")
 public class TutorialController {
