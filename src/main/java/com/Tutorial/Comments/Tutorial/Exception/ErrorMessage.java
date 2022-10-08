94b55fb718a8a64d68ad8764e65c7066be9d6421
William Spearman <wspearman@protonmail.com>
2022-10-08 15:23:56 -0400

    Implemented Error Message for customized message.

--- src/main/java/com/Tutorial/Comments/Tutorial/Exception/ErrorMessage.java
+++ src/main/java/com/Tutorial/Comments/Tutorial/Exception/ErrorMessage.java
@@ -1,4 +1,30 @@
 package com.Tutorial.Comments.Tutorial.Exception;
 
+import java.util.Date;
+
 public class ErrorMessage {
+    // members /////////////////////////////////////////////////////////////////////////////////
+    private int statusCode;
+    private Date timestamp;
+    private String message;
+    private String description;
+    
+    // methods /////////////////////////////////////////////////////////////////////////////////
+      public ErrorMessage(
+        int statusCode,
+        Date timestamp,
+        String message,
+        String description) {
+          this.statusCode  = statusCode;
+          this.timestamp   = timestamp;
+          this.message     = message;
+          this.description = description;
+      }
+    
+      // getters and setters ///////////////////////////////////////////////////////////////////
+      public int getStatusCode() { return statusCode; }
+      public Date getTimestamp() {return timestamp; }
+      public String getMessage() {return message; }
+      public String getDescription() { return description;}
+     
 }
