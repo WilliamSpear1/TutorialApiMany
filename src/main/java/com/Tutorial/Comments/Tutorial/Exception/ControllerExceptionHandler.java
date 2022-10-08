7ccf6def42af1b191ed8363f4555b712fc963b3b
William Spearman <wspearman@protonmail.com>
2022-10-08 15:23:05 -0400

    Implemented Controller Exception Handler for customized error message.

--- src/main/java/com/Tutorial/Comments/Tutorial/Exception/ControllerExceptionHandler.java
+++ src/main/java/com/Tutorial/Comments/Tutorial/Exception/ControllerExceptionHandler.java
@@ -1,4 +1,35 @@
 package com.Tutorial.Comments.Tutorial.Exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import java.util.Date;
+
+@RestControllerAdvice
 public class ControllerExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorMessage> resourceNotFoundException(ResourceNotFoundException notFoundException, WebRequest request) {
+        ErrorMessage message = new ErrorMessage(
+          HttpStatus.NOT_FOUND.value(),
+          new Date(),
+          notFoundException.getMessage(),
+          request.getDescription(false)
+          );
+        
+        return new ResponseEntity<ErrorMessage>(message, HttpStatus.NOT_FOUND);
+    }
+    
+   @ExceptionHandler(Exception.class)
+   public ResponseEntity<ErrorMessage> globalExceptionHandler(Exception exception, WebRequest request) {
+        ErrorMessage message = new ErrorMessage(
+          HttpStatus.INTERNAL_SERVER_ERROR.value(),
+          new Date(),
+          exception.getMessage(),
+          request.getDescription(false)
+        );
+        
+        return new ResponseEntity<ErrorMessage>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+   }
 }
