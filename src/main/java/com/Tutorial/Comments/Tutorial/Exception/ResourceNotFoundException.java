0ca6b9c3f3955d32454360cb149472b0a66d8e9b
William Spearman <wspearman@protonmail.com>
2022-10-08 15:28:10 -0400

    Created a class for customized exception when a tutorial can not be found.

--- src/main/java/com/Tutorial/Comments/Tutorial/Exception/ResourceNotFoundException.java
+++ src/main/java/com/Tutorial/Comments/Tutorial/Exception/ResourceNotFoundException.java
@@ -1,4 +1,9 @@
 package com.Tutorial.Comments.Tutorial.Exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
